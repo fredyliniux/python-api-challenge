4790a35648d0c23a0a569cc9375fca278ffed348
Fredy Reyes <32718596+fredyliniux@users.noreply.github.com>
2021-06-25 03:37:29 -0700

    Delete api_keys.py

--- starter_code/api_keys.py
+++
@@ -1,7 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "168a62a15a4d334c775252936860d459"
-
-# Google API Key
-g_key = "AIzaSyAzepGU1-wvW5QWldmIeLACW03zVELMy3E"
-
-key = "AIzaSyAzepGU1-wvW5QWldmIeLACW03zVELMy3E"
